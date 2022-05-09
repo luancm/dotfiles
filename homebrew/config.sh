82e24a38f7cffe1dcdb0d0930f24b2445d5cdd0d
Luan Martins <luan.martins@talkdesk.com>
2022-05-09 09:36:44 +0100

    Add macos homebrew configurations
    Also improve logging

---
+++ homebrew/config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# config for homebrew
+
+# always use a Homebrew-installed curl rather than the system version.
+# https://github.com/Homebrew/brew/issues/5563
+export HOMEBREW_FORCE_BREWED_CURL=1

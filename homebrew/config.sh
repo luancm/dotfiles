2843f10d0f99531b738a70df7787f4d1857db15f
luancm <luan.martins@talkdesk.com>
2022-10-28 18:34:58 +0100

    add shellenv for homebrew command in PATH

--- homebrew/config.sh
+++ homebrew/config.sh
@@ -5,3 +5,4 @@
 # always use a Homebrew-installed curl rather than the system version.
 # https://github.com/Homebrew/brew/issues/5563
 export HOMEBREW_FORCE_BREWED_CURL=1
+eval $(/opt/homebrew/bin/brew shellenv)

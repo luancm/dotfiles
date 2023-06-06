0badba7447b49fe89a9406bdf67ab967e5432349
luancm <luan.martins@talkdesk.com>
2023-06-06 16:46:56 +0100

    feat: add branches alias for git
    The goal is to show a table with all the local branches ordered by the last commit date

--- git/aliases.sh
+++ git/aliases.sh
@@ -4,3 +4,5 @@ git config --global alias.st status
 git config --global alias.co checkout
 
 git config --global alias.lsd "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+git config --global alias.branches "! git for-each-ref --sort=-committerdate refs/heads --format='%(HEAD)%(color:yellow)%(refname:short)|%(color:bold green)%(committerdate:relative)|%(color:blue)%(subject)|%(color:magenta)%(authorname)%(color:reset)' --color=always | column -s\| -t"

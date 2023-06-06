6cdc697090d3187f021e05910c4aa9995fe79574
luancm <luan.martins@talkdesk.com>
2023-06-06 16:45:18 +0100

    chore: improve compatibility of brew_upgrade script

--- homebrew/upgrade.sh
+++ homebrew/upgrade.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
 if [ -z $DOTFILES ]; then echo 'Dotfiles were not installed, to install run `source ~/.dotfiles/install`'; return 1; fi
 source $DOTFILES/lib/io_handlers.sh;
 
 log_info 'Homebrew: Updating';
-brew update --verbose;
+brew update;
 log_info 'Homebrew: Upgrading';
-if [ $(brew outdated --quiet | grep -v gamemaker) ]; then
+if [ "$(brew outdated --quiet | grep -v gamemaker)" ]; then
     brew upgrade $(brew outdated --quiet | grep -v gamemaker);
     log_success 'Homebrew: Successfully upgraded';
 else

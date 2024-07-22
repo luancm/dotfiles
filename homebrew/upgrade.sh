6510786e45c4b6bc55cae028c6d16e31b3562b10
luancm <luan.martins@sky.uk>
2024-07-22 15:00:45 +0100

    feat: add parameter for brew upgrade

--- homebrew/upgrade.sh
+++ homebrew/upgrade.sh
@@ -3,11 +3,13 @@
 if [ -z $DOTFILES ]; then echo 'Dotfiles were not installed, to install run `source ~/.dotfiles/install`'; return 1; fi
 source $DOTFILES/lib/io_handlers.sh;
 
+opts=$1
+
 log_info 'Homebrew: Updating';
 brew update;
 log_info 'Homebrew: Upgrading';
-if [ "$(brew outdated --quiet | grep -v gamemaker)" ]; then
-    brew upgrade $(brew outdated --quiet | grep -v gamemaker);
+if [ "$(brew outdated --quiet $opts | grep -v gamemaker | grep -v slack)" ]; then
+    brew upgrade $(brew outdated --quiet $opts | grep -v gamemaker | grep -v slack);
     log_success 'Homebrew: Successfully upgraded';
 else
     log_success 'Homebrew: Nothing to upgrade';

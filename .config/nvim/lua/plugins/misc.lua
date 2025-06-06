39a386725216f3bf4b7e8510e41b18b7fa5b7b6e
luancm <lawncm@gmail.com>
2025-06-06 17:52:51 +0100

    add hardtime.nvim for me to suffer

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -2,4 +2,13 @@ return {
   { "ThePrimeagen/vim-be-good" },
   { "nvim-tree/nvim-web-devicons", opts = {} },
   { "folke/todo-comments.nvim",    event = "VimEnter", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
+  {
+    "m4xshen/hardtime.nvim",
+    opts = {},
+    config = function()
+      require("hardtime").setup({})
+
+      vim.keymap.set("n", "<leader>H", "<cmd>Hardtime toggle<cr>")
+    end,
+  },
 }

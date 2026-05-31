1f591ec94e2b871340af6caf275f0a0e492dcfd3
Luan Martins <lawncm@gmail.com>
2026-05-31 13:40:09 +0100

    guard homebrew shellenv path for intel macs

--- homebrew/config.sh
+++ homebrew/config.sh
@@ -5,6 +5,12 @@
 # always use a Homebrew-installed curl rather than the system version.
 # https://github.com/Homebrew/brew/issues/5563
 export HOMEBREW_FORCE_BREWED_CURL=1
-eval $(/opt/homebrew/bin/brew shellenv)
+
+# Apple Silicon installs to /opt/homebrew, Intel to /usr/local.
+if [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
 
 source "$DOTFILES/homebrew/aliases.sh"

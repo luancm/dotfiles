56d92e61c53314e8b7cb6d098d46c174df80ba58
Luan Martins <lawncm@gmail.com>
2026-06-30 14:58:34 +0100

    add tmux aliases and main session auto-attach

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,6 +11,16 @@ alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true 
 alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
 alias docker_killall_ps='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 
+alias pxmlab='ssh -t homelab \~/.local/bin/pxm'
+
+# tmux
+alias tma="tmux attach"                  # attach to the most-recent session
+alias tmat="tmux attach -t"              # attach to a named one: tmat work
+alias tms="tmux new-session -s"          # new named session: tms work
+alias tml="tmux ls"                      # list sessions
+alias tmk="tmux kill-session -t"         # kill a named session: tmk work
+alias tmm="tmux new-session -A -s main"  # jump to (or create) the main session
+
 if ! $is_mac_os; then
     alias reload!="exec $(getent passwd $(id -un) | awk -F : '{print $NF}') -l"
     alias ls="ls -F --color"
